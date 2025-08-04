41204f8dff69cd6c0d49d1d0e342fcc54a87f5e7
loremixx <laine31415@gmail.com>
2025-08-04 13:37:49 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+const words = [
+  "AI (UNHLAB)",
+  "WFP",
+  "UNODC",
+  "CHAMBER OF DEPUTIES",
+  "UNWOMEN",
+  "SHADOW SECURITY COUNCIL",
+  "F-UNDRR",
+  "COM"
+];
+
+const span = document.querySelector(".typing-text span");
+let index = 0;
+
+function showNextWord() {
+  span.textContent = words[index];
+  span.style.color = "#e4b97d"; 
+  index = (index + 1) % words.length;
+}
+
+showNextWord();
+setInterval(showNextWord, 800);
